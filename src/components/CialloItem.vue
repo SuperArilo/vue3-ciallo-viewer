ebcc7a5ac766a829279dad1ad1233dc38b234c47
SuperArilo <superarilowh@gmail.com>
2024-09-14 17:40:55 +0800

    更改缩放样式规律

--- src/components/CialloItem.vue
+++ src/components/CialloItem.vue
@@ -26,8 +26,8 @@ const centerPosition = {
     y: 0
 }
 const style = ref<CSSProperties>({
-    transform: `matrix(1, 0, 0, 1, ${props.preX}, ${props.preY})`,
-    transition: BuildTransition.value([{ type: 'transform', duration: props.duration }]),
+    transform: `translate(${props.preX}px, ${props.preY}px)`,
+    transition: BuildTransition.value([{ type: 'transform', duration: props.duration }, { type: 'width', duration: props.duration }, { type: 'height', duration: props.duration }]),
     width: `${props.rawObject.clientWidth == 0 ? 208:props.rawObject.clientWidth}px`,
     height: `${props.rawObject.clientHeight == 0 ? 200:props.rawObject.clientHeight}px`
 })
@@ -40,20 +40,25 @@ const handleError = (e: Event): void => {
 const reSetImageStatus = (): void => {
     if(instance.value == null) return
     instance.value.style.transition = ''
-    ratio.value = ImageRatio(instance.value)
-    centerPosition.x = window.innerWidth / 2 - instance.value.clientWidth / 2
-    centerPosition.y = window.innerHeight / 2 - instance.value.clientHeight / 2
-    style.value.transform = `matrix(${ratio.value}, 0, 0, ${ratio.value}, ${centerPosition.x}, ${centerPosition.y})`
+    ratio.value = ImageRatio(props.rawObject)
+    centerPosition.x = window.innerWidth / 2 - props.rawObject.clientWidth * ratio.value / 2
+    centerPosition.y = window.innerHeight / 2 - props.rawObject.clientHeight * ratio.value / 2
+    window.requestAnimationFrame(() => {
+        style.value.width = `${props.rawObject.clientWidth * ratio.value}px`
+        style.value.height = `${props.rawObject.clientHeight * ratio.value}px`
+        style.value.transform = `translate(${centerPosition.x}px, ${centerPosition.y}px)`
+    })
+
 }
 defineExpose({
     reSetImageStatus
 })
-watch(() => [props.x, props. y], e => {
+watch(() => [props.x, props.y], e => {
     if(e) {
         window.requestAnimationFrame(() => {
             if(instance.value !== null) {
-                instance.value.style.transform = `matrix(${ratio.value}, 0, 0, ${ratio.value}, ${centerPosition.x + props.x}, ${centerPosition.y + props.y})`
-                instance.value.style.transition = e[0] === 0 && e[1] === 0 ? BuildTransition.value([{ type: 'opacity', duration: props.duration }, { type: 'transform', duration: props.duration }]) : ''
+                style.value.transform = `translate(${centerPosition.x + props.x}px, ${centerPosition.y + props.y}px)`
+                style.value.transition = e[0] === 0 && e[1] === 0 ? BuildTransition.value([{ type: 'opacity', duration: props.duration }, { type: 'transform', duration: props.duration }]) : ''
             }
         })
     }
@@ -62,19 +67,16 @@ watch(() => [props.x, props. y], e => {
 watch(() => props.status, e => {
     if(!e && props.index === props.targetIndex) {
         window.requestAnimationFrame(() => {
-            if(errorStatus) {
-                if(instance.value == null) return
-                style.value.transform = `matrix(0.001, 0, 0, 0.001, ${window.innerWidth / 2 - instance.value.width / 2}, ${window.innerHeight / 2 - instance.value.height / 2})`
-            } else {
-                style.value.transform = `matrix(1, 0, 0, 1, ${props.preX}, ${props.preY})`
-            }
+            style.value.transition = BuildTransition.value([{ type: 'transform', duration: props.duration }, { type: 'width', duration: props.duration }, { type: 'height', duration: props.duration }])
+            style.value.transform = `translate(${props.preX}px, ${props.preY}px)`
+            style.value.width = `${props.rawObject.clientWidth}px`
+            style.value.height = `${props.rawObject.clientHeight}px`
         })
     }
 })
 </script>
 <style scoped>
 img {
-    width: 0;
-    height: 0;
+    transform-origin: center;
 }
 </style>
