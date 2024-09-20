cf45a386a88ada46970f8d2942c17260a43f1167
SuperArilo <superarilowh@gmail.com>
2024-09-20 15:10:27 +0800

    重新翻写图片浏览逻辑

--- src/components/CialloItem.vue
+++ src/components/CialloItem.vue
@@ -1,83 +1,92 @@
 <template>
-    <img
-        ref="instance"
-        :style="style"
-        :src="props.src"
-        @load="handleLoad"
-        @error="handleError"
-        alt="" />
+    <div class="view_instance" :style="BoxStyle">
+        <img
+            ref="instance"
+            :style="style"
+            :src="props.src"
+            @load="handleLoad"
+            @error="handleError"
+            alt="" />
+    </div>
 </template>
 <script setup lang="ts">
 import {CialloItemProps} from "../type/Types"
 import {BuildTransition, ImageRatio} from "../util/PublicFunction"
-import {CSSProperties, ref, watch} from "vue"
-import {errorPng} from "../util/PublicData"
+import {errorPng} from '../util/PublicData'
+import {CSSProperties, ref, watch, onBeforeMount, onMounted} from "vue"
 const props = withDefaults(defineProps<CialloItemProps>(), {
     duration: 300,
     status: true
 })
+let errorStatus:boolean = false
+const BoxStyle = ref<CSSProperties>({
+    transform: '',
+    transition: BuildTransition.value([{ type: 'transform', duration: props.duration }])
+})
 const instance = ref<HTMLImageElement | null>(null)
-//图片是否有效
-let errorStatus: boolean = false
-let ratio = ref<number>(0)
+const WHRatio = ref<number>(0)
+const aspectRatio = ref<number>(props.rawObject.naturalWidth / props.rawObject.naturalHeight)
+const preInstanceRatio = ref<number>(0)
 //记录的图片位于屏幕上的中心坐标
 const centerPosition = {
     x: 0,
     y: 0
 }
 const style = ref<CSSProperties>({
-    transform: `translateX(${props.preX}px) translateY(${props.preY}px) translateZ(0)`,
-    transition: BuildTransition.value([{ type: 'transform', duration: props.duration }, { type: 'width', duration: props.duration }, { type: 'height', duration: props.duration }]),
-    width: `${props.rawObject.clientWidth == 0 ? 208:props.rawObject.clientWidth}px`,
-    height: `${props.rawObject.clientHeight == 0 ? 200:props.rawObject.clientHeight}px`
+    width: '',
+    height: ''
 })
-const handleLoad = (): void => reSetImageStatus()
-const handleError = (e: Event): void => {
-    const target = e.target as HTMLImageElement
-    target.src = errorPng
+const handleLoad = (event: Event): void => {
+    const element = event.currentTarget as HTMLImageElement
+    if(errorStatus) {
+        aspectRatio.value = element.naturalWidth / element.naturalHeight
+        PreInitFunction(element)
+    }
+    window.requestAnimationFrame(() => {
+        BoxStyle.value.transform = `matrix(1, 0, 0, 1, ${centerPosition.x}, ${centerPosition.y})`
+    })
+}
+const handleError = (event: Event): void => {
+    const element = event.currentTarget as HTMLImageElement
+    element.src = errorPng
     errorStatus = true
 }
-const setErrorPng = (): void => {
-    if(instance.value == null) return
-    ratio.value = ImageRatio(instance.value)
-    centerPosition.x = window.innerWidth / 2 - instance.value.clientWidth * ratio.value / 2
-    centerPosition.y = window.innerHeight / 2 - instance.value.clientHeight * ratio.value / 2
-    window.requestAnimationFrame(() => {
-        if(instance.value == null) return
-        style.value.transform = `translateX(${centerPosition.x}px) translateY(${centerPosition.y}px) translateZ(0)`
-    })
+const PreInitFunction = (element: HTMLImageElement): void => {
+    WHRatio.value = ImageRatio(element)
+    const width = element.clientWidth * WHRatio.value
+    const height = width / aspectRatio.value
+    if(width == 0 || height == 0) return
+    style.value.width = `${width}px`
+    style.value.height = `${height}px`
+    centerPosition.x = window.innerWidth / 2 - width / 2
+    centerPosition.y = window.innerHeight / 2 - height / 2
+    preInstanceRatio.value = 1 / WHRatio.value * props.rawObject.clientHeight / props.rawObject.clientHeight
 }
 const reSetImageStatus = (): void => {
     if(instance.value == null) return
-    instance.value.style.transition = ''
-    if(errorStatus) {
-        setErrorPng()
-        return
+    if(BoxStyle.value.transition !== '') {
+        BoxStyle.value.transition = ''
     }
-    const clientWidth = props.rawObject.clientWidth
-    const clientHeight = props.rawObject.clientHeight
-    ratio.value = ImageRatio(props.rawObject)
-    if(!errorStatus) {
-        centerPosition.x = window.innerWidth / 2 - clientWidth * ratio.value / 2
-        centerPosition.y = window.innerHeight / 2 - clientHeight * ratio.value / 2
-    }
-    window.requestAnimationFrame(() => {
-        style.value.width = `${clientWidth * ratio.value}px`
-        style.value.height = `${clientHeight * ratio.value}px`
-        style.value.transform = `translateX(${centerPosition.x}px) translateY(${centerPosition.y}px) translateZ(0)`
-    })
-
+    PreInitFunction(instance.value)
+    BoxStyle.value.transform = `matrix(1, 0, 0, 1, ${centerPosition.x}, ${centerPosition.y})`
 }
+onBeforeMount(() => {
+    PreInitFunction(props.rawObject)
+    BoxStyle.value.transform = `matrix(${preInstanceRatio.value}, 0, 0, ${preInstanceRatio.value}, ${props.preX}, ${props.preY})`
+})
+onMounted(() => {
+    window.requestAnimationFrame(() => {
+        BoxStyle.value.transform = `matrix(1, 0, 0, 1, ${centerPosition.x}, ${centerPosition.y})`
+    })
+})
 defineExpose({
     reSetImageStatus
 })
 watch(() => [props.x, props.y], e => {
     if(e) {
         window.requestAnimationFrame(() => {
-            if(instance.value !== null) {
-                style.value.transform = `translateX(${centerPosition.x + e[0]}px) translateY(${centerPosition.y + e[1]}px) translateZ(0)`
-                style.value.transition = e[0] === 0 && e[1] === 0 ? BuildTransition.value([{ type: 'opacity', duration: props.duration }, { type: 'transform', duration: props.duration }]) : ''
-            }
+            BoxStyle.value.transform = `matrix(1, 0, 0, 1, ${centerPosition.x + e[0]}, ${centerPosition.y + e[1]})`
+            BoxStyle.value.transition = e[0] === 0 && e[1] === 0 ? BuildTransition.value([{ type: 'opacity', duration: props.duration }, { type: 'transform', duration: props.duration }]) : ''
         })
     }
 
@@ -85,17 +94,31 @@ watch(() => [props.x, props.y], e => {
 watch(() => props.status, e => {
     if(!e && props.index === props.targetIndex) {
         window.requestAnimationFrame(() => {
-            style.value.transition = BuildTransition.value([{ type: 'transform', duration: props.duration }, { type: 'width', duration: props.duration }, { type: 'height', duration: props.duration }])
-            style.value.transform = `translateX(${props.preX}px) translateY(${props.preY}px) translateZ(0)`
-            style.value.width = `${props.rawObject.clientWidth}px`
-            style.value.height = `${props.rawObject.clientHeight}px`
+            PreInitFunction(props.rawObject)
+            const p = (errorStatus ? 0.01:preInstanceRatio.value).toFixed(4)
+            BoxStyle.value.transition = BuildTransition.value([{ type: 'transform', duration: props.duration }])
+            BoxStyle.value.transform = `matrix(${p}, 0, 0, ${p}, ${props.preX}, ${props.preY})`
         })
     }
 })
 </script>
 <style scoped>
-img {
-    object-fit: cover;
-    will-change: width, height, transform;
+.view_instance {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    transform-origin: left top;
+    overflow: hidden;
+}
+.view_instance img {
+    max-width: none;
+    -webkit-user-drag: none;
+    cursor: grab;
+}
+.view_instance img:active {
+    cursor: grabbing;
 }
 </style>
