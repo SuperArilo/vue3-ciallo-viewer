0f5213db20524c16bb3b607b4fbd5e9180a09408
SuperArilo <superarilowh@gmail.com>
2024-10-16 16:05:49 +0800

    添加 zoomSpeed 缩放速度

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import {createVNode, render} from 'vue'
 import ListViewer from "./CialloViewer.vue"
 let container: HTMLElement
-export const CialloViewer = (array: HTMLCollection, targetIndex: number | null = 0, duration: number = 400): void => {
+export const CialloViewer = (
+    array: HTMLCollection,
+    targetIndex: number | null = 0,
+    duration: number = 400,
+    zoomSpeed: number = 0.2): void => {
     if(array.length == 0) return
     if(container == null) {
         container = document.createElement('div')
@@ -9,7 +13,7 @@ export const CialloViewer = (array: HTMLCollection, targetIndex: number | null =
         container.setAttribute('id', `ciallo-viewer-${(Math.random() * 100000).toFixed(0)}`)
         document.body.appendChild(container)
     }
-    const vNode = createVNode(ListViewer, { images: array, targetIndex: targetIndex, duration: duration })
+    const vNode = createVNode(ListViewer,{ images: array, targetIndex: targetIndex, duration: duration, zoomSpeed: zoomSpeed })
     render(vNode, container)
 }
 export const UnmountTargetViewer = (): void => render(null, container)
