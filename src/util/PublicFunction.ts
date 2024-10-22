8a446df2b7a6720de85718db4d979bdc903efa78
SuperArilo <superarilowh@gmail.com>
2024-10-22 14:27:19 +0800

    remove

--- src/util/PublicFunction.ts
+++ src/util/PublicFunction.ts
@@ -15,6 +15,5 @@ export const SetElementStyle = (style: CSSProperties, element: HTMLElement): voi
         }
     })
 }
-export const ImageRatio = (current: HTMLImageElement): number => Math.min(window.innerWidth / current.clientWidth, window.innerHeight / current.clientHeight)
 export const BuildMatrix = (first: number | string, second: number | string, third: number | string, forth: number | string, fifth: number | string, sixth: number | string): string => `matrix(${first}, ${second}, ${third}, ${forth}, ${fifth}, ${sixth})`
 export const isCloseTo = (value: number, target: number, epsilon: number = 1e-10): boolean => Math.abs(value - target) < epsilon
