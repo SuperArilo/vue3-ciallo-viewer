1cc1a1f4f32a50d0e03e5edc6d6d9100d928859b
SuperArilo <superarilowh@gmail.com>
2024-10-28 10:17:17 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -14,14 +14,12 @@ export default defineConfig({
         outDir: 'dist',
         sourcemap: true,
         lib: {
-            entry: './src/index.ts', // 确保 entry 文件路径是正确的
+            entry: './src/index.ts',
             name: 'vue3-ciallo-viewer'
         },
         rollupOptions: {
-            // 确保外部化处理那些你不想打包进库的依赖
             external: ['vue'],
             output: {
-                // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
                 globals: {
                     vue: 'Vue',
                 },
